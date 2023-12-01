01a683c481eea4bb5bac121d76b7576a28bda8d9
ajsobrino <ajsobrino@izertis.com>
2023-12-01 16:44:19 +0100

    My final solution for first problem

--- 1-Day/findFirstRepeated.ts
+++ 1-Day/findFirstRepeated.ts
@@ -1,13 +1,11 @@
 function findFirstRepeated(gifts :number[]): number {
     let setValues:Set<Number> = new Set<Number>();
-    let result = -1;
     for (let item of gifts){
         if (setValues.has(item)){
-            result = item;
-            break;
+            return item;
         }else{
             setValues.add(item)
         }
     }
-    return result
+    return -1
 }
