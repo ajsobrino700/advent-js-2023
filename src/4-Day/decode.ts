987c8663b3f56a09b745ad334e3bbca75b8ee783
ajsobrino <ajsobrino@izertis.com>
2023-12-05 15:14:49 +0100

    Remove and clean snippet of code

--- src/4-Day/decode.ts
+++ src/4-Day/decode.ts
@@ -3,7 +3,8 @@ function decode(message: string): string {
   let i=0
   let decodeWord = (message:string):string => {
     let result = '';
-    while( i < size ){
+    while( i < message.length ){
+      
       if(message.charAt(i) === '('){
         i++;
         result = result + decodeWord(message)
@@ -13,16 +14,12 @@ function decode(message: string): string {
       }else{
         result+=message.charAt(i)
         i++;
-      } 
+      }
+      
     }
+    
     return result;
   }
   let result = decodeWord(message)
   return result
 }
-
-
-
-
-//console.log(decode('hola (odnum)'))
-console.log(decode('sa(u(cla)atn)s'))
